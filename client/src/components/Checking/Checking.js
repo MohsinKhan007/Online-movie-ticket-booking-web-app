9e899f5809ac110a0f641c7631570f1f1e305adc
MohsinKhan007 <mohsinwaseem65@gmail.com>
2022-10-10 14:25:38 +0200

    Restoring main branch to proper working react verison and dependencies

---
+++ client/src/components/Checking/Checking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+function Checking(){
+    return(<>
+    
+    <button data-testid="todo-1" id="todo-1">Hello world</button>
+    
+    </>);
+}
+
+export default Checking;
